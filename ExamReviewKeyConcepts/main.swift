7ee37b72d66f55b0521de76b85976d313ce72d6a
Russell Gordon <rgordon@rsgc.on.ca>
2017-05-13 18:26:07 -0400

    Initial Commit

---
+++ ExamReviewKeyConcepts/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExamReviewKeyConcepts
+//
+//  Created by Russell Gordon on 5/13/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
